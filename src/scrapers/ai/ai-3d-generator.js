72c81f0b6c285bfb0bbf09187050071e3a9779ac
SxyzZenith <sxyzz.dev@gmail.com>
2025-02-23 06:04:06 +0700

    Add

---
+++ src/scrapers/ai/ai-3d-generator.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+async function Ai3dGenerator(prompt) {
+  try {
+    let { data } = await axios.get(`https://api.artvy.ai:444/image_search?query=${encodeURIComponent(prompt)} t5 Deepart Concept`, {
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Cache-Control": "no-cache",
+        "Pragma": "no-cache",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive"
+      }
+    });
+    return JSON.stringify(data, null, 2);
+  } catch (error) {
+    console.error("Error fetching data:", error.response ? error.response.data : error.message);
+    return null;
+  }
+}
+
+// Ai3dGenerator("A Woman And Sunset").then(console.log)
+
+export default Ai3dGenerator;
