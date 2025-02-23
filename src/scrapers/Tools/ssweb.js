221c16afcf2bdadf815b9d3639d7256637bb52ab
SxyzZenith <sxyzz.dev@gmail.com>
2025-02-23 18:13:11 +0700

    Add

--- src/scrapers/Tools/ssweb.js
+++ src/scrapers/Tools/ssweb.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-async function ssweb(url, formatOutput, outputPaths) {
-   let { data } = await axios.get(`https://api.pikwy.com/?tkn=125&d=3000&u=${url}&fs=0&w=1280&h=1200&s=100&z=100&f=${formatOutput}&rt=jweb`)
+async function ssweb(options={}) {
+   let { data } = await axios.get(`https://api.pikwy.com/?tkn=125&d=3000&u=${options.url}&fs=0&w=1280&h=1200&s=100&z=100&f=${options.extOutput}&rt=jweb`)
    return data
 }
 
-// ssweb("https://chatgpt.com", "jpg","output.jpg").then(console.log)
+// ssweb({
+//   url: "https://chatgpt.com",
+//   extOutput: "png"
+// }).then(console.log)
 
 export default ssweb;
